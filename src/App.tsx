030709f67c44d62ff28dde8b61eca68c434e3689
intersect88 <intersect88@users.noreply.github.com>
2026-02-03 12:32:47 +0100

    style: split venue button into name + location on two lines

--- src/App.tsx
+++ src/App.tsx
@@ -141,12 +141,15 @@ function App() {
                 rel="noopener noreferrer"
                 className="link-button venue-link"
               >
-                <div>
+                <div className="venue-content">
                   <svg className="icon-svg" viewBox="0 0 24 24" fill="currentColor">
                     <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/>
                   </svg>
-                  Ammot - Castel Volturno (CE)
-                  <p style={{fontSize: '1.1rem', marginTop: '0.3rem', opacity: 0.8}}>Clicca per raggiungere il locale</p>
+                  <div className="venue-text">
+                    <span className="venue-name">Ammot</span>
+                    <span className="venue-location">Castel Volturno (CE)</span>
+                    <p className="venue-note" style={{fontSize: '1.1rem', marginTop: '0.3rem', opacity: 0.8}}>Clicca per raggiungere il locale</p>
+                  </div>
                 </div>
               </a>
 
